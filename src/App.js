1d331242d16a6274f9eb9e1431214eaa380331c4
nanoton <fabianosrb@gmail.com>
2022-01-10 00:48:18 -0400

    Adicionando projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ListaDeNotas from './componentes/listaDeNotas';
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder='Título' />
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
+  );
+}
+
+export default App;
