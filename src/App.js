a94fa7fee31bb2d73b4260282e905f7e816ab8c5
nanoton <fabianosrb@gmail.com>
2022-01-10 20:57:30 -0400

    Estilizando

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./componentes/ListaDeNotas.jsx/listaDeNotas";
-import  FormularioCadastro  from "./componentes/FormularioDeNotas/FormularioCadastro";
+import  FormularioCadastro  from "./componentes/FormularioDeNotas";
 import "./assets/App.css";
 import "./assets/index.css";
+import ListaDeNotas from "./componentes/ListaDeNotas";
 
 class App extends Component {
   render() {
     return (
       <section>
         <FormularioCadastro />
-        <ListaDeNotas />
+        <ListaDeNotas/>
       </section>
     );
   }
