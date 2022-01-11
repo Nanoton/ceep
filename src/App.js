d568634f1fb7450f96025f9d54ca08f8d7037324
nanoton <fabianosrb@gmail.com>
2022-01-11 19:42:44 -0400

    Cards adicionaveis

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import ListaDeNotas from "./componentes/ListaDeNotas";
 
 class App extends Component {
 
+  constructor(){
+    super();
+    this.state={
+      notas:[]
+    };
+  }
   criarNota(titulo, texto){
-    console.log('uma nova nota foi criada ' + titulo + " " + texto)
+    const novaNota = {titulo,texto};
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section>
-        <FormularioCadastro criarNota={this.criarNota}/>
-        <ListaDeNotas/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
